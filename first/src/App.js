3f200837bde8086cc922e4000ac064c6042c03e0
Vishvas Patel <pvishvas7@gmail.com>
2023-02-18 12:09:04 +0530

    react useeffect and state added

--- first/src/App.js
+++ first/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+const [counter,setCounter] = useState(0)
+const increment = ()=>{
+    setCounter(counter +1)
+}
+
+useEffect(()=>{
+  document.title = `clicked ${counter} times`
+},[counter])
   return (
     <div className="App">
-      <title>react practice</title>
+      
     <h1>trying react basic commands</h1>
+      <h2>{counter}</h2>
+    <button onClick={increment}>click here!</button>
     </div>
   );
 }
